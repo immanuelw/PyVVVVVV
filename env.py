41511e3d8ad6021ef275fddedd47bc0c6607b908
immanuelw <immwa@sas.upenn.edu>
2015-11-12 19:35:27 -0500

    testing image change

--- env.py
+++ env.py
@@ -10,8 +10,9 @@ and rendering data needed to render that one tile, along with a
 collection of all the entities in that tile.
 '''
 import pygame
-from pygame.locals import *
+#from pygame.locals import *
 import config as cf
+from img import img_dict
 
 class Environment(object):
 	def __init__(self, area, geometry, image, background, entities):
@@ -21,6 +22,7 @@ class Environment(object):
 		self.background = background
 		self.entities = set(entities)
 		self.characters = set()
+		self.checkpoints = set()
 		self.dodebugdraw = False
 		for ent in self.entities:
 			#Call it a hack...
@@ -29,6 +31,8 @@ class Environment(object):
 				ent.rect.ent = ent
 			elif ent.enttype == cf.ENT_CHARACTER:
 				self.characters.add(ent)
+			elif ent.enttype == cf.ENT_CHECKPOINT:
+				self.checkpoints.add(ent)
 
 	def draw(self, surf):
 		self.background.draw(surf)
@@ -50,5 +54,11 @@ class Environment(object):
 		self.entities.discard(ent)
 
 	def update(self):
+		char = tuple(self.characters)[0]
 		for ent in self.entities:
+			#if ent in self.checkpoints:
+			#	if ent.rect.bottom == char.rect.bottom + 16 and char.rect.left <= ent.rect.right and char.rect.left >= ent.rect.left:
+			#		ent.image = img_dict['./data/img/checkpoint.png']
+			#	else:
+			#		ent.image = img_dict['./data/img/checkpointBW.png']
 			ent.update(self.area, self)
